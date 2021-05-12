a00e5eae619dc33c862c9bb9d6804e50da91070c
Alyssa DiCarlo <alyssa.dicarlo@gmail.com>
2021-05-11 23:13:16 -0400

    added menu class

--- pet_exercise/main.py
+++ pet_exercise/main.py
@@ -1,6 +1,7 @@
 from pet import Pet, CuddlyPet
 from toy import Toy
 from treat import Treat, ColdPizza, Bacon, VeganSnack
+from menu import Menu
 
 # Begin with no pets.
 pets = []
@@ -15,30 +16,6 @@ main_menu = [
     "Do nothing",
 ]
 
-def print_menu_error():
-    print("That was not a valid choice. Try again.\n\n\n")    
-
-def choices_to_string(choice_list):
-    choice_string = ""
-    num = 1
-    for choice in choice_list:
-        choice_string += "%d: %s\n" % (num, choice)
-        num += 1
-    choice_string += "Please choose an option: "
-    return choice_string
-
-def get_user_choice(choice_list):
-    choice = -1
-    choice_string = choices_to_string(choice_list)
-    while choice == -1:
-        try:
-            choice = int(input(choice_string))
-            if choice <= 0 or choice > len(choice_list):
-                raise ValueError
-        except ValueError:
-            print_menu_error()
-    return choice
-
 adoption_menu = [
     "Pet",
     "Cuddly Pet"
@@ -50,13 +27,15 @@ treat_menu = [
     "VeganSnack"
 ]
 
-def main():    
+def main():
+    main = Menu("Please choose an option:", main_menu)
+    type = Menu("Please choose the type of pet:", adoption_menu)
+    treat = Menu("Please choose the type of treat:", treat_menu)
     while True:
-        choice = get_user_choice(main_menu)
+        choice = main.get_choice()
         if choice == 1:
             pet_name = input("What would you like to name your pet? ")
-            print("Please choose the type of pet: ")
-            type_choice = get_user_choice(adoption_menu)
+            type_choice = type.get_user_choice()
             if type_choice == 1:
                 pets.append(Pet(pet_name))
             elif type_choice == 2:
@@ -75,16 +54,15 @@ def main():
             for pet in pets:
                 pet.get_toy(Toy())
         if choice == 6:
-            print("Please choose the type of treat: ")
-            treat_choice = get_user_choice(treat_menu)
+            treat_choice = treat.get_user_choice()
             if treat_choice == 1:
-                treat = ColdPizza()
+                treat_type = ColdPizza()
             if treat_choice == 2:
-                treat = Bacon()
+                treat_type = Bacon()
             if treat_choice == 3:
-                treat = VeganSnack
+                treat_type = VeganSnack
             for pet in pets:
-                pet.get_treat(treat)
+                pet.get_treat(treat_type)
         if choice == 7:
             for pet in pets:
                 pet.be_alive()
